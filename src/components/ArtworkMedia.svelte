5c8658239d64dfbadbe3e938d64670e764e61d95
Adam Soltys <asoltys@gmail.com>
2022-03-03 22:21:36 -0800

    fix card links and thumbnails

--- src/components/ArtworkMedia.svelte
+++ src/components/ArtworkMedia.svelte
@@ -12,7 +12,7 @@
   export let popup = false;
   let img, vid;
   let path;
-  $: if (thumb) {
+  $: if (thumb && artwork.title.startsWith("Harvest")) {
     if ($painting || $variation || $edition) {
       path = `/api/public/${artwork.filename}.mp4`;
     } else {
