64c1fb9a0b13efcd554186d85a4f52174711467e
Adam Soltys <asoltys@gmail.com>
2022-03-30 09:07:20 -0700

    change font color on artwork page

--- src/routes/a/[slug].svelte
+++ src/routes/a/[slug].svelte
@@ -650,8 +650,7 @@
       <Sidebar bind:artwork />
 
       {#if artwork.description}
-        <div
-          class="mob-desc description text-gray-600 whitespace-pre-wrap break-words">
+        <div class="mob-desc description whitespace-pre-wrap break-words">
           <h4 class="mt-10 font-bold">About this artwork</h4>
           <div class="desc-text {showMore ? 'openDesc' : ''}">
             {@html linkify(artwork.description)}
@@ -691,7 +690,7 @@
       </div>
 
       {#if artwork.description}
-        <div class="desk-desc description text-gray-600 break-words">
+        <div class="desk-desc description break-words">
           <h4 class="mt-10 mb-5 font-bold">About this artwork</h4>
           <div class="whitespace-pre-wrap">
             {@html linkify(artwork.description)}
